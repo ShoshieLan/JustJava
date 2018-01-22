194d1f5255f7f2b4335dbbbdcc6dab218acfeb42
Shoshie Lan <shoshielan@gmail.com>
2018-01-22 14:26:12 -0500

    finish lesson 5

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -18,14 +18,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        //int quantity = 2;
-        display(quantity);
-        displayPrice(quantity * 5);
+        String priceMessage = "Total: $" + quantity * 5 + "\n" +"Thank you!";
+        displayMessage(priceMessage);
+    }
+
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 
 
     public void increment(View view) {
-      //  int quantity = 3;
+        //  int quantity = 3;
         quantity++;
         displayQuantity(quantity);
     }
@@ -40,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
+
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
